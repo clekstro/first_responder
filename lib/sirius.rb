fc134ade10409de2c23cb0168320befeef68ba33
Curtis Ekstrom <curtis@wellmatchhealth.com>
2013-06-16 19:25:11 -0400

    Classes can set attributes on themselves, json must parse

--- lib/sirius.rb
+++ lib/sirius.rb
@@ -1,29 +1,42 @@
 require "sirius/version"
-require "sirius/object_from_json"
-require "sirius/object_from_xml"
 require 'virtus'
 require 'aequitas'
 
 module Sirius
   FORMATS = [:json, :xml]
 
-  def initialize(format=:json, serialized)
-    @format = ensure_format(format)
+  module InstanceMethods
+    def initialize(format=:json, serialized)
+      @format = ensure_format(format)
+      @data = deserialize(serialized)
+      # set_attributes_for(@data)
+    end
+
+    def ensure_format(fmt)
+      return fmt if FORMATS.include?(fmt)
+      raise UnknownSerializationFormat
+    end
+
+    def deserialize(serialized)
+      return JSON.parse(serialized) if @format == :json
+    end
   end
 
-  def ensure_format(fmt)
-    return fmt if FORMATS.include?(fmt)
-    raise UnknownSerializationFormat
-  end
-
-  def self.included base
-    base.extend Virtus
-    base.extend Aequitas
-    base.extend Sirius::Exceptions
+  module ClassMethods
+    include Virtus
+    include Aequitas
+    def requires(attribute, type, opts={})
+      attribute(attribute, type, opts)
+    end
   end
 
   module Exceptions
     class UnknownSerializationFormat < Exception; end
   end
 
+  def self.included base
+    base.extend Sirius::Exceptions
+    base.send(:include, Sirius::InstanceMethods)
+    base.extend Sirius::ClassMethods
+  end
 end
--- spec/sirius_spec.rb
+++ spec/sirius_spec.rb
@@ -5,12 +5,34 @@ describe Sirius do
   describe ".initialize" do
     let(:klass) { class Test; include Sirius; end }
 
-    context 'with unknown format' do
+    context "with unknown format" do
       it "raises UnknownSerializationFormat error" do
         expect{ klass.new(:wrong, '') }.to raise_error
       end
     end
 
+    context "with valid format" do
+      context "and valid data" do
+        it "initializes successfully" do
+          expect{ klass.new(:json, '{"foo":"bar"}') }.not_to raise_error
+        end
+      end
+      context "and invalid data" do
+        it "blows up" do
+          expect{ klass.new(:json, '') }.to raise_error
+        end
+      end
+    end
 
+    describe "required single variables" do
+      let(:klass) {
+        class Test
+          include Sirius
+          requires :foo, String
+        end
+      }
+      subject { klass }
+      it{ should respond_to(:foo) }
+    end
   end
 end
